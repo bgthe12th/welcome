ab4ffa6566726b37c16f6759eaf23ae5a5bb572c
Ben G <bgthe12th@gmail.com>
2018-05-21 09:24:45 -0500

    4.2 Updated layout for Products page, added Size and Price to Options

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_20_155428) do
+ActiveRecord::Schema.define(version: 2018_05_21_131106) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2018_05_20_155428) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "size"
+    t.decimal "price"
   end
 
 end
