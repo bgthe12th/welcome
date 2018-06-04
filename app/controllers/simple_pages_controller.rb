d749317018503483346837c491e8c1c6a64c808d
Ben G <bgthe12th@gmail.com>
2018-06-04 13:51:01 -0500

    4.6 Created Orders Table & Database. Display order details.

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -2,7 +2,7 @@ class SimplePagesController < ApplicationController
   def index
   end
   def landing_page
-      @products = Product.limit(3)
+      @products = Product.limit(4)
   end
 
 end
--- db/migrate/20180604161624_create_orders.rb
+++ db/migrate/20180604161624_create_orders.rb
@@ -1,11 +1,10 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.integer :user_id
       t.integer :product_id
-      t.float :total
+      t.decimal :total
+
+      t.timestamps
     end
-    add_index :orders, :user_id
-    add_index :orders, :product_id
   end
 end
